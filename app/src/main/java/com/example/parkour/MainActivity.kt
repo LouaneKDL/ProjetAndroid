154e365844aab6a33b3a17e2ceb1a4416756fe71
Victor <victoryvray@gmail.com>
2025-03-14 11:41:21 +0100

    :sparkles: Ajout de la vue competition fonctionnelle

--- app/src/main/java/com/example/parkour/MainActivity.kt
+++ app/src/main/java/com/example/parkour/MainActivity.kt
@@ -5,7 +5,9 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.ViewModelProvider
@@ -16,19 +18,20 @@ import com.example.parkour.viewModel.CoursesViewModel
 import com.example.parkour.viewModel.ObstaclesViewModel
 import com.example.parkour.viewModel.PerformanceObstaclesViewModel
 import com.example.parkour.viewModel.PerformancesViewModel
-import com.example.parkour.views.CompetitionView
+import com.example.parkour.views.Competition
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val competitionViewModel = ViewModelProvider(this)[PerformancesViewModel::class.java]
+        val competitionViewModel = ViewModelProvider(this)[CompetitionViewModel::class.java]
         enableEdgeToEdge()
         setContent {
             ParkourTheme {
-                Surface(Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    CompetitionView(competitionViewModel)
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Competition(
+                        modifier = Modifier.padding(innerPadding),
+                        competitionViewModel
+                    )
                 }
             }
         }
--- app/src/main/java/com/example/parkour/views/CompetitionView.kt
+++ app/src/main/java/com/example/parkour/views/CompetitionView.kt
@@ -1,15 +1,24 @@
 package com.example.parkour.views
 
+import android.annotation.SuppressLint
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.OutlinedTextField
@@ -22,7 +31,14 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.parkour.R
 import com.example.parkour.viewModel.CompetitionViewModel
 import com.example.parkour.viewModel.CompetitorsViewModel
 import com.example.parkour.viewModel.CoursesViewModel
@@ -30,36 +46,122 @@ import com.example.parkour.viewModel.ObstaclesViewModel
 import com.example.parkour.viewModel.PerformanceObstaclesViewModel
 import com.example.parkour.viewModel.PerformancesViewModel
 
+
+@SuppressLint("ResourceType")
 @Composable
-fun CompetitionView(viewModel: PerformancesViewModel){
+fun Competition(modifier: Modifier = Modifier, viewModel: CompetitionViewModel) {
 
-    val competitions by viewModel.performances.observeAsState(emptyList())
+    val competitions by viewModel.competitions.observeAsState(emptyList())
+    viewModel.getData()
 
-    Column(modifier = Modifier.fillMaxSize().padding(8.dp),
-        horizontalAlignment = Alignment.CenterHorizontally){
-        Row(
-            modifier = Modifier.fillMaxWidth().padding(8.dp),
-            verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.Absolute.SpaceEvenly
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .background(Color.LightGray),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        Text(
+            text = "Compétitions",
+            modifier = modifier.padding(10.dp),
+            fontSize = 23.sp,
+            fontWeight = FontWeight.Bold
+        )
+
+        Button(
+            modifier = Modifier.padding(10.dp),
+            onClick = {},
+            colors = ButtonColors(
+                Color.Black,
+                contentColor = Color.White,
+                disabledContainerColor = Color.Gray,
+                disabledContentColor = Color.White
+            ),
+            enabled = false
         ){
-            IconButton(onClick = {
-                viewModel.getData()
-            }) {
-                Icon(imageVector = Icons.Default.Search,
-                    contentDescription = "search competition")
+            Text(
+                text = "Ajouter une compétition",
+                modifier = modifier,
+                color = Color.White
+            )
+        }
+
+        Column(
+            modifier = modifier
+                .clip(shape = RoundedCornerShape(8.dp))
+                .background(Color.White)
+                .padding(8.dp)
+
+        ){
+
+            LazyColumn {
+                for (compet in competitions){
+                    item{
+                        LazyRow(
+                            verticalAlignment = Alignment.CenterVertically,
+                            modifier = Modifier
+                                .width(300.dp)
+                                .border(width = 1.dp, color = Color.Black).padding(10.dp),
+                            horizontalArrangement = Arrangement.SpaceBetween
+                        ) {
+
+                            item{
+
+                                Column {
+                                    Text(
+                                        text = "➣  " + compet.name,
+                                        fontSize = 15.sp,
+                                        fontWeight = FontWeight.Bold
+                                    )
+                                    Text(
+                                        text = "        • " + compet.gender,
+                                        fontSize = 13.sp
+                                    )
+                                    Text(
+                                        text = "        • " + compet.age_min + " à " + compet.age_max,
+                                        fontSize = 13.sp
+                                    )
+                                }
+                            }
+                            item{
+                                Column{
+                                    Button(
+                                        onClick = {},
+                                        colors = ButtonColors(
+                                            Color.Black,
+                                            contentColor = Color.White,
+                                            disabledContainerColor = Color.Gray,
+                                            disabledContentColor = Color.White
+                                        )
+                                    ) {
+                                        Image(
+                                            imageVector = ImageVector.vectorResource(R.drawable.baseline_people_alt_24),
+                                            contentDescription = "concurrents"
+                                        )
+                                    }
+                                    Button(
+                                        onClick = {},
+                                        colors = ButtonColors(
+                                            Color.Black,
+                                            contentColor = Color.White,
+                                            disabledContainerColor = Color.Gray,
+                                            disabledContentColor = Color.White
+                                        )
+                                    ) {
+                                        Image(
+                                            imageVector = ImageVector.vectorResource(R.drawable.baseline_info_24),
+                                            contentDescription = "parkours"
+                                        )
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
             }
         }
 
-        LazyColumn {
-            items(competitions) { comp ->
-                Text(
-                    text = comp.status,
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(8.dp)
-                )
-            }
-        }
+
+
     }
 
 }
