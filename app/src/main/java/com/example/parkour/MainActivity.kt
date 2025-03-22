ac44d32fe1f6de1af784ce1465c557f52d82105b
Léo-Paul <marie2328@unicaen.fr>
2025-03-22 17:45:40 +0100

    :sparkles: Création de la vue inscription de concurrents

--- app/src/main/java/com/example/parkour/MainActivity.kt
+++ app/src/main/java/com/example/parkour/MainActivity.kt
@@ -9,8 +9,12 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.parkour.ui.theme.ParkourTheme
 import com.example.parkour.viewModel.CompetitionViewModel
 import com.example.parkour.viewModel.CompetitorsViewModel
@@ -19,19 +23,42 @@ import com.example.parkour.viewModel.ObstaclesViewModel
 import com.example.parkour.viewModel.PerformanceObstaclesViewModel
 import com.example.parkour.viewModel.PerformancesViewModel
 import com.example.parkour.views.Competition
+import com.example.parkour.views.CompetitorRegistration
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val competitionViewModel = ViewModelProvider(this)[CompetitionViewModel::class.java]
+        val competitorsViewModel = ViewModelProvider(this)[CompetitorsViewModel::class.java]
         enableEdgeToEdge()
         setContent {
             ParkourTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    val navController = rememberNavController()
+                    NavHost(navController = navController, startDestination = Routes.competitionView, builder = {
+
+                        composable(Routes.competitionView){
+                            Competition(
+                                modifier = Modifier.padding(innerPadding),
+                                competitionViewModel,
+                                navController
+                            )
+                        }
+
+                        composable(Routes.competitorRegistrationView){
+                            CompetitorRegistration(
+                                modifier = Modifier.padding(innerPadding),
+                                competitorsViewModel,
+                                navController
+                            )
+                        }
+                    })
+                    /*
                     Competition(
                         modifier = Modifier.padding(innerPadding),
                         competitionViewModel
                     )
+                    */
                 }
             }
         }
--- app/src/main/java/com/example/parkour/views/CompetitorRegistrationView.kt
+++ app/src/main/java/com/example/parkour/views/CompetitorRegistrationView.kt
@@ -1,35 +1,23 @@
 package com.example.parkour.views
 
-import android.annotation.SuppressLint
-import android.util.Log
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonColors
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.livedata.observeAsState
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -41,20 +29,12 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.example.parkour.R
-import com.example.parkour.Routes
-import com.example.parkour.viewModel.CompetitionViewModel
 import com.example.parkour.viewModel.CompetitorsViewModel
-import com.example.parkour.viewModel.CoursesViewModel
-import com.example.parkour.viewModel.ObstaclesViewModel
-import com.example.parkour.viewModel.PerformanceObstaclesViewModel
-import com.example.parkour.viewModel.PerformancesViewModel
 
-
-@SuppressLint("ResourceType")
 @Composable
-fun Competition(modifier: Modifier = Modifier, viewModel: CompetitionViewModel, navController: NavController) {
+fun CompetitorRegistration(modifier: Modifier = Modifier, viewModel: CompetitorsViewModel, navController: NavController) {
 
-    val competitions by viewModel.competitions.observeAsState(emptyList())
+    val competitors by viewModel.competitors.observeAsState(emptyList())
     viewModel.getData()
 
     Column(
@@ -62,14 +42,14 @@ fun Competition(modifier: Modifier = Modifier, viewModel: CompetitionViewModel, 
             .fillMaxSize()
             .background(Color.LightGray),
         horizontalAlignment = Alignment.CenterHorizontally
-    ){
+    ) {
         Text(
-            text = "Compétitions",
+            text = "Concurrents",
             modifier = modifier.padding(10.dp),
             fontSize = 23.sp,
             fontWeight = FontWeight.Bold
         )
-
+        /*
         Button(
             modifier = Modifier.padding(10.dp),
             onClick = {},
@@ -80,13 +60,14 @@ fun Competition(modifier: Modifier = Modifier, viewModel: CompetitionViewModel, 
                 disabledContentColor = Color.White
             ),
             enabled = false
-        ){
+        ) {
             Text(
                 text = "Ajouter une compétition",
                 modifier = modifier,
                 color = Color.White
             )
         }
+        */
 
         Column(
             modifier = modifier
@@ -94,44 +75,42 @@ fun Competition(modifier: Modifier = Modifier, viewModel: CompetitionViewModel, 
                 .background(Color.White)
                 .padding(8.dp)
 
-        ){
+        ) {
 
             LazyColumn {
-                for (compet in competitions){
-                    item{
+                for (competitor in competitors) {
+                    item {
                         LazyRow(
                             verticalAlignment = Alignment.CenterVertically,
                             modifier = Modifier
                                 .width(300.dp)
-                                .border(width = 1.dp, color = Color.Black).padding(10.dp),
+                                .border(width = 1.dp, color = Color.Black)
+                                .padding(10.dp),
                             horizontalArrangement = Arrangement.SpaceBetween
                         ) {
 
-                            item{
+                            item {
 
                                 Column {
                                     Text(
-                                        text = "➣  " + compet.name,
+                                        text = "➣  " + competitor.last_name + " " + competitor.first_name,
                                         fontSize = 15.sp,
                                         fontWeight = FontWeight.Bold
                                     )
                                     Text(
-                                        text = "        • " + compet.gender,
+                                        text = "        • " + competitor.gender,
                                         fontSize = 13.sp
                                     )
                                     Text(
-                                        text = "        • " + compet.age_min + " à " + compet.age_max,
+                                        text = "        • " + competitor.born_at,
                                         fontSize = 13.sp
                                     )
                                 }
                             }
-                            item{
-                                Column{
+                            item {
+                                Column {
                                     Button(
-                                        onClick = {
-                                            navController.navigate(Routes.competitorRegistrationView)
-                                            // Log.i("ugazuhfiuhfiuaho :", "blabla")
-                                        },
+                                        onClick = {},
                                         colors = ButtonColors(
                                             Color.Black,
                                             contentColor = Color.White,
@@ -166,8 +145,5 @@ fun Competition(modifier: Modifier = Modifier, viewModel: CompetitionViewModel, 
             }
         }
 
-
-
     }
-
 }
